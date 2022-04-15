d8f483a9758c9b04bab137610db25336248ffc36
DavidJTemple <96357555+DavidJTemple@users.noreply.github.com>
2022-04-15 07:46:18 -0230

    Daves change now

--- services/census.dal.js
+++ services/census.dal.js
@@ -18,15 +18,10 @@ const getAllCensus = () => {
 const getCensusByFamilyName = (family_name) => {
   console.log("Outside promise");
   return new Promise(function (resolve, reject) {
-<<<<<<< HEAD
     app.set("view-engine", "ejs");
     console.log("Inside promise: Family name: " + family_name);
     const sql = "SELECT * FROM census1 WHERE family_name = $1";
     dal.query(sql, [family_name], (err, result) => {
-=======
-    const sql = "SELECT * FROM census WHERE family_name = $1";
-    dal.query(sql, [id], (err, result) => {
->>>>>>> bc5792f0e4be0d99134786c815dadaa434e137f3
       if (err) {
         reject(err);
       } else {
