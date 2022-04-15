020d3b47a01aae8043d36a60e31ba63c0ff1274d
DavidJTemple <96357555+DavidJTemple@users.noreply.github.com>
2022-04-15 08:02:34 -0230

    Fixed changes

--- routes/prov_search.js
+++ routes/prov_search.js
@@ -5,16 +5,8 @@ router.use(express.static("public"));
 
 const censusDal = require("../services/census.dal");
 
-<<<<<<< HEAD
 router.get("/", (req, res) => {
   res.render("prov_search.ejs");
 });
 
 module.exports = router;
-=======
-router.get("/",  (req, res) =>{
-    res.render("prov_search.ejs")
-  })
-
-  module.exports = router;
->>>>>>> bc5792f0e4be0d99134786c815dadaa434e137f3
